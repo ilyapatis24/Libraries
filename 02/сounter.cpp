c6c12c9bc5bdd9e7e98d69dd57b25c90caf91602
ilyapatis24 <ilyapatis24@mail.ru>
2023-12-02 00:17:39 +0300

    change file names

---
+++ 02/сounter.cpp
@@ -0,0 +1,23 @@
+#include "ñounter.h"
+
+
+Counter::Counter() {
+	counter_var = 0;
+}
+Counter::Counter(int counter_value) {
+	counter_var = counter_value;
+}
+
+Counter::~Counter() {}
+
+int Counter::increment_counter() {
+	return ++counter_var;
+}
+
+int Counter::decrement_counter() {
+	return --counter_var;
+}
+
+int Counter::print_counter_state() {
+	return counter_var;
+}
